127a1ab9290458956424bd45997a77230760f8f4
Camilo Campaz J <1005897813@u.icesi.edu.co>
2023-04-23 20:04:40 -0500

    First commit 🎈

---
+++ src/translate/languages/pt.js
@@ -0,0 +1,116 @@
+const StringsComuns = {
+  pt: {
+      components: {
+          about: {
+            titulo: "Una conexío rapida y sencilla entre estudiantes y empresas",
+            paragrafo:"Una plataforma que te permite dar el siguiente paso en tu carrera profesional y encontrar el trabajo de tus sueños.",
+            buttons: {
+              chrome: "Saber más",
+              firefox: "Contactanos",
+            },
+          },
+          contact: {
+            tituloPrincipal: "5.000+ ya se han unido",
+            titulo: "Mantente al día con lo último en tecnología y ofertas laborales",
+            botao: "Contact Us",
+            textoBotao: "Enter your email address",
+          },
+          extension: {
+            titulo: "Descarga nuestra extensión",
+            paragrafo:"We’ve got more browsers in the pipeline. Please do let us know if you’ve got a favourite you’d like us to prioritize",
+              button: "Add & Install Extension",
+            chrome: {
+              titulo: "Add to Chrome",
+              paragrafo: "Minimum version 62",
+             
+            },
+            firefox: {
+              titulo: "Add to Firefox",
+              paragrafo: "Minimum version 55",
+              
+            },
+            opera: {
+              titulo: "Add to Opera",
+              paragrafo: "Minimum version 46",
+              
+            },
+          },
+          features: {
+            titulo: "Beneficios de la plataforma",
+            paragrafo:"Las mejores funciones de nuestra plataforma",
+            button: "Más información",
+            menu: {
+              opcao1: "Busqueda sencilla",
+              opcao2: "Busqueda inteligente",
+              opcao3: "Comparte tu información",
+            },
+           card1: {
+            titulo: "Busquedas con un solo click",
+            paragrafo:"Busqueda rápida, completa y mediante filtros. Encuentra el trabajo de tus sueños en un solo click.",
+            
+            },
+            card2: {
+              titulo: "Busqueda inteligente",
+              paragrafo:"Nuestro poderoso motor de busqueda mediante inteligencia artificial te permite encontrar el trabajo que mejor se adapte a tus necesidades.",
+             
+              },
+              card3: {
+                titulo: "Comparte tu información",
+                paragrafo:"Facilmente comparte tu hoja de vida con las empresas que te interesen. Además nuestros asesores te ayudarán a mejorarla.",
+                },
+              },
+        rotas: {
+          features: "FEATURES",
+          pricing:"PRICING",
+          contact:"CONTACT",
+          login:"LOGIN",
+          },
+          login: {
+            tituloLogin: "Inicio de sesión",
+            tituloCriar: "Crear una cuenta",
+            entrar:"Login",
+            titulo: "FEATURES",
+            navegacao:{
+              criar:"Crear una cuenta",
+              entrar:"Login",
+            },
+            inputs:{
+              nome:"How do you want to be called?",
+              cpf:"Enter your CPF",
+              telefone:"Enter Phone",
+              email:"Enter your e-mail",
+              senha:"Type your password",
+              confirmarSenha:"Confirm the password" ,
+            },
+            },
+            questions:{
+              titulo:"Preguntas frecuentes",
+              paragrafo:"Aquí encontrarás las respuestas a las preguntas más frecuentes sobre Jobfinder. Si no encuentras la respuesta que buscas, no dudes en contactarnos.",
+              pergunta1:{
+                titulo:"Que es Jobfinder?",
+                resposta:"Jobfinder es una red social profesional que conecta a empleadores y candidatos en busca de oportunidades laborales. A través de Jobfinder, los usuarios pueden crear un perfil detallado de su experiencia y habilidades, buscar y aplicar a trabajos relevantes, y establecer conexiones valiosas en su campo de interés. Jobfinder es una herramienta poderosa para desarrollar y avanzar en una carrera profesional, y es ampliamente utilizada por empresas y profesionales en todo el mundo.",
+              },
+              pergunta2:{
+                titulo:"Que hace especial a Jobfinder?",
+                resposta:"Lo que hace a Jobfinder una plataforma única es su capacidad de facilitar la conexión entre empleadores y candidatos de forma efectiva y eficiente. A través de su plataforma, los usuarios pueden buscar trabajos relevantes, aplicar a ellos, y crear perfiles profesionales detallados para mostrar sus habilidades y experiencia.",
+              },
+              pergunta3:{
+                titulo:"A quienes está orientado?",
+                resposta:"Jobfinder está orientado a profesionales de todo el mundo pero aún mas a estudiantes prontos a egresar o recien egresados que buscan oportunidades laborales. A través de su plataforma, los usuarios pueden crear perfiles profesionales detallados, buscar y aplicar a trabajos relevantes, y establecer conexiones valiosas en su campo de interés.",
+              },
+              pergunta4:{
+                titulo:"Cómo puedo estar en Jobfinder?",
+                resposta:"Para estar en Jobfinder, primero debes crear una cuenta. Para crear una cuenta, debes ingresar a la página de inicio de sesión y seleccionar la opción de crear una cuenta. Luego, debes ingresar tu nombre, tu correo electrónico y una contraseña. Una vez que hayas completado el formulario, haz clic en el botón de crear cuenta. ¡Listo! Ahora puedes comenzar a usar Jobfinder.",
+              },
+            },
+      
+            sucessoEmail:{
+              titulo:"Email successfully registered",
+              paragrafo:"Enjoy more content on our landing page, Enjoy more content on our landing page, Enjoy more content on our landing page.",
+              button:"Back to Home",
+            },
+        },
+  }
+}
+
+export { StringsComuns }
