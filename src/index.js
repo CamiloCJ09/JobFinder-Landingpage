2248a41904d71e47a48ec9ece7ac891becd6dc96
Camilo Campaz J <1005897813@u.icesi.edu.co>
2023-05-08 07:13:51 -0500

    Feat: Adding analytics 🎈

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-
+import { inject } from '@vercel/analytics';
+ 
+inject();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
