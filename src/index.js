c8f432adc0b2b4b7633dc05ca3838ffa3a10fb50
Camilo Campaz J <1005897813@u.icesi.edu.co>
2023-05-08 07:17:05 -0500

    Fix: Changing vercel import 🎈

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { inject } from '@vercel/analytics';
- 
+
+import { Analytics } from '@vercel/analytics/react';
+
 inject();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
+    <Analytics mode={'production'} />;
     <App />
   </BrowserRouter>
 );
